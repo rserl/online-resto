ce86179db896d2d19ce53ca0c0a732b75a9fe164
riskaserli <riskaserlii@gmail.com>
2023-04-08 02:24:33 +0700

    [FEATURE] add OutOfStockException

--- src/main/java/com/example/onlineresto/utils/exception/OutOfStockException.java
+++ src/main/java/com/example/onlineresto/utils/exception/OutOfStockException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class OutOfStockException extends Exception{
+public class OutOfStockException extends RuntimeException{
     public  OutOfStockException(String message) {
         super(message);
     }
